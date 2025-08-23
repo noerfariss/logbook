9b60a87513981b3521ffb8157af6f667f277b7e9
NUR FARIS PRASTYO <noerfaris@gmail.com>
2025-08-23 08:31:42 +0700

    log

--- app/Models/PengajuanDeadline.php
+++ app/Models/PengajuanDeadline.php
@@ -5,12 +5,37 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Agent\Agent;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\Contracts\Activity;
 
 class PengajuanDeadline extends Model
 {
+    use LogsActivity;
+
     protected $connection = 'alternative';
     protected $table = 'logbook_pengajuan_deadlines';
     protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['pengajuan_id', 'deadline', 'created_at', 'updated_at'])
+            ->useLogName('user')
+            ->logOnlyDirty();
+    }
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $agent = new Agent();
+        $activity->properties = $activity->properties->merge([
+            'ip' => request()->ip(),
+            'browser' => $agent->browser(),
+            'browser_version' => $agent->version($agent->browser()),
+            'platform' => $agent->platform(),
+        ]);
+    }
 
     protected function deadline(): Attribute
     {
--- app/Models/PengajuanPpn.php
+++ app/Models/PengajuanPpn.php
@@ -3,10 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Agent\Agent;
+use Spatie\Activitylog\Contracts\Activity;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class PengajuanPpn extends Model
 {
+    use LogsActivity;
+
     protected $connection = 'alternative';
     protected $table = 'logbook_pengajuan_ppns';
     protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['pengajuan_id', 'status', 'created_at', 'updated_at'])
+            ->useLogName('user')
+            ->logOnlyDirty();
+    }
+
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $agent = new Agent();
+        $activity->properties = $activity->properties->merge([
+            'ip' => request()->ip(),
+            'browser' => $agent->browser(),
+            'browser_version' => $agent->version($agent->browser()),
+            'platform' => $agent->platform(),
+        ]);
+    }
 }
